5a1095106b510ffe3003e7ce58fd01b6a8f20a87
Kevin Tatooles <tatooles@wisc.edu>
2022-08-02 22:04:32 -0500

    Bot now uses google sheets to store last id and works properly

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,6 +1,11 @@
 import tweepy
+import gspread
 
-#bearer_token = 'AAAAAAAAAAAAAAAAAAAAAHTmXgEAAAAAUNuZWKqsMgfmv4LWXlzkIQQ9X7A%3DzWWjCD4BqF7vao3fswhdpa3G3wMkJH1Jk5AOYPqxbuBuADomcg'
+gc = gspread.service_account('credentials.json')
+
+# Open a sheet from a spreadsheet in one go
+wks = gc.open("last-tweet-id").sheet1
+
 consumerKey = 'IODvXnxsaiG8cU7MtY4XZhyLN'
 consumerSecret = 'R9uqkxEXxQdmYFxFwdOSIqibi17WLqDH45QDAC337uC6lYcRWQ'
 
@@ -13,28 +18,20 @@ client = tweepy.Client(consumer_key=consumerKey, consumer_secret=consumerSecret,
 ## Better than a different type of string because that could randomly be part of the tweet
 ## Random string would probably work though
 
-FILE_NAME = 'last_seen_id.txt'
+def storeId(last_seen_id):
+    wks.update('A2', str(last_seen_id))
 
-def storeId(last_seen_id, filename):
-    with open(filename, 'w') as file:
-        file.write(str(last_seen_id))
+def retrieveId():
+    return int(wks.acell('A2').value)
 
-def retrieveId(filename):
-    with open(filename, 'r') as file:
-        id = int(file.read().strip())
-    return id
-
-lastId = retrieveId(FILE_NAME)
+lastId = retrieveId()
 response = client.get_users_mentions(id=1478092305361952769, since_id=lastId, expansions=['author_id'], user_auth=True)
 tweets = response.data
-
-# users = {user['id'] : user for user in users}
 if tweets:
     for tweet in reversed(tweets):
         lastId = tweet.id
-        storeId(lastId, FILE_NAME)
+        storeId(lastId)
         tokens = tweet.text.lower().split()
         if tokens[1] == 'echo':
-            print("echoing")
             # Echo their tweet
             client.create_tweet(text=tweet.text, in_reply_to_tweet_id=tweet.id)
