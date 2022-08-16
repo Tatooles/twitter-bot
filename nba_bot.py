b74b4413a383053718e16f2534fdf703f4da0364
Kevin Tatooles <tatooles@wisc.edu>
2022-08-15 22:19:15 -0500

    Starting on nba stats aspect of bot

--- nba_bot.py
+++ nba_bot.py
@@ -1,5 +1,6 @@
 import tweepy
 import gspread
+import sys
 
 gc = gspread.service_account('credentials.json')
 
@@ -14,15 +15,20 @@ accessToken_secret = 'lFwfVInbbgJOQ1gmgMmr7U7jHn7ZLygTTcPGrRXJhPg5G'
 
 client = tweepy.Client(consumer_key=consumerKey, consumer_secret=consumerSecret, access_token=accessToken, access_token_secret=accessToken_secret)
 
-## Idea: maybe use a hashtag to control what the bot will return
-## Better than a different type of string because that could randomly be part of the tweet
-## Random string would probably work though
-
 def storeId(last_seen_id):
     wks.update('A2', str(last_seen_id))
 
 def retrieveId():
     return int(wks.acell('A2').value)
+
+# This function takes in a request string from the user, and returns what the bot will respond with
+def return_stats(request_string):
+    # String format @sports_guesses SPORT PLAYER_FIRSTNAME PLAYER_LASTNAME STAT YEAR
+    # Example: @sports_guesses NBA Lebron James ppg 2021-22
+    tokens = request_string.split()
+    if(tokens[1] != 'NBA'):
+        return 'ERROR - I could not process your request. Reason: Invalid sport. Please request a valid sport. Currently supported sports: NBA'
+    return ''
 
 lastId = retrieveId()
 response = client.get_users_mentions(id=1478092305361952769, since_id=lastId, expansions=['author_id'], user_auth=True)
@@ -31,7 +37,8 @@ if tweets:
     for tweet in reversed(tweets):
         lastId = tweet.id
         storeId(lastId)
-        tokens = tweet.text.lower().split()
-        if tokens[1] == 'echo':
-            # Echo their tweet
-            client.create_tweet(text=tweet.text, in_reply_to_tweet_id=tweet.id)
+        tweet_text = return_stats(tweet.text.lower())
+        client.create_tweet(text=tweet_text, in_reply_to_tweet_id=tweet.id)
+
+if __name__ == '__main__':
+    print(return_stats(sys.argv[1]))
