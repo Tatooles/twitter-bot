bcf9cb5cba777110cfe53e22c70e36851cc8085f
Kevin Tatooles <tatooles@wisc.edu>
2022-08-20 00:31:21 -0500

    Updated tests

--- test_bot.py
+++ test_bot.py
@@ -14,9 +14,13 @@ class TestCalc(unittest.TestCase):
     def test_return_stats(self):
         result1 = nba_bot.process_request('@sportstatsgenie NBA Zach LaVine 2020-21 PTS')
         result2 = nba_bot.process_request('@sportstatsgenie NBA Michael Jordan 1997-98 PTS')
+        result3 = nba_bot.process_request('@sportstatsgenie nba kyle korver 2005-06 fg3_pct')
+        result4 = nba_bot.process_request('@SPORTSTATSGENIE NBA J.R. REID 1997-98 PLUS_MINUS')
         
-        self.assertEqual(result1, 'Zach LaVine averaged 27.4 PTS in the 2020-21 season')
-        self.assertEqual(result2, 'Michael Jordan averaged 28.7 PTS in the 1997-98 season')
+        self.assertEqual(result1, 'Zach Lavine averaged 27.4 pts in the 2020-21 season')
+        self.assertEqual(result2, 'Michael Jordan averaged 28.7 pts in the 1997-98 season')
+        self.assertEqual(result3, 'Kyle Korver averaged 0.42 fg3_pct in the 2005-06 season')
+        self.assertEqual(result4, 'J.r. Reid averaged 0.3 plus_minus in the 1997-98 season')
 
     def test_errors(self):
         result1 = nba_bot.process_request('@randomat NBA Zach LaVine 2020-21 PTS')
@@ -26,7 +30,11 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(result2, 'ERROR - I could not process your request. Please request a valid sport. Currently supported sport: NBA')
 
     def test_invalid_query(self):
-        result1 = nba_bot.process_request('')
+        result1 = nba_bot.process_request('@sportstatsgenie NBA Jim Jameson 2020-21 PTS')
+        result2 = nba_bot.process_request('@sportstatsgenie NBA Zach LaVine 2020-21 blks')
+
+        self.assertEqual(result1, 'ERROR - I could not process your request. Couldn\'t complete a valid query with the information you provided')
+        self.assertEqual(result2, 'ERROR - I could not process your request. Couldn\'t complete a valid query with the information you provided')
 
 
 if __name__ == '__main__':
