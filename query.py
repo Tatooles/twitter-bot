a778413946b861c892e1a609a64ec4e2811ee0ac
Kevin Tatooles <tatooles@wisc.edu>
2022-09-13 22:47:36 -0500

    Update readme and some comments

--- query.py
+++ query.py
@@ -20,3 +20,15 @@ cursor.execute("SELECT * FROM test_table_2")
 myresult = cursor.fetchall()
 
 print(myresult)
+
+'''
+To add all the data to the database it might be smart to just pull it into a dataframe
+
+nba_stats = gc.open("nba-stats").sheet1
+df = pd.DataFrame(nba_stats.get_all_records())
+
+and put that dataframe into the database as shown here:
+https://www.projectpro.io/recipes/connect-mysql-python-and-import-csv-file-into-mysql-and-create-table
+
+Would save a big hassle of working with credentials of the CSV
+'''
