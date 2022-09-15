8b318ec668e43354c5404e390f339cb1c2f80755
Kevin Tatooles <tatooles@wisc.edu>
2022-09-14 22:57:08 -0500

    Finally successfully brought the data into the db

--- query.py
+++ query.py
@@ -16,94 +16,118 @@ with open('railway_credentials.json') as credentials:
         port=creds['port']
     )
 
-# gc = gspread.service_account('gspread_credentials.json')
-# nba_stats = gc.open("nba-stats").sheet1
-# df = pd.DataFrame(nba_stats.get_all_records())
+gc = gspread.service_account('gspread_credentials.json')
+nba_stats = gc.open("nba-stats").sheet1
+df = pd.DataFrame(nba_stats.get_all_records())
 
-# print(df.head)
-
-cursor = mydb.cursor()
 t0 = time.time()
-#cursor.execute("SELECT * FROM nbastats")
-cursor.execute("insert into nbastats values(1, stephen curry, steph, 1, gsw, 33, 82, 82, 0, .999, 33.42, 33.42, 33.42, .523, 33.42, 33.42, .523, 33.42, 33.42, .523, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 12.52, 1, 1, 30.52, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2345, 2021-22)")
+cursor = mydb.cursor()
 
-myresult = cursor.fetchall()
-t1 = time.time()
-print(myresult)
-print(t1-t0)
-
-#cursor.execute("CREATE TABLE nbastats(player_id INT, player_name VARCHAR(255), nickname VARCHAR(255), team_id INT, team_abbreviation VARCHAR(3), age INT, gp INT, w INT, l INT, w_pct DECIMAL(3,3), min DECIMAL(5,2), fgm DECIMAL(5,2), fga DECIMAL(5,2), fg_pct DECIMAL(3,3), fg3m DECIMAL(5,2), fg3a DECIMAL(5,2), fg3_pct DECIMAL(3,3), ftm DECIMAL(5,2), fta DECIMAL(5,2), ft_pct DECIMAL(3,3), oreb DECIMAL(5,2), dreb DECIMAL(5,2), reb DECIMAL(5,2), ast DECIMAL(5,2), tov DECIMAL(5,2), stl DECIMAL(5,2), blk DECIMAL(5,2), blka DECIMAL(5,2), pf DECIMAL(5,2), pfd DECIMAL(5,2), pts DECIMAL(5,2), plus_minus DECIMAL(5,2), nba_fantasy_pts DECIMAL(5,2), dd2 INT, td3 INT, wnba_fantasy_pts DECIMAL(5,2), gp_rank INT, w_rank INT, l_rank INT, w_pct_rank INT, min_rank INT, fgm_rank INT, fga_rank INT, fg_pct_rank INT, fg3m_rank INT, fg3a_rank INT, fg3_pct_rank INT, ftm_rank INT, fta_rank INT, ft_pct_rank INT, oreb_rank INT, dreb_rank INT, reb_rank INT, ast_rank INT, tov_rank INT, stl_rank INT, blk_rank INT, blka_rank INT, pf_rank INT, pfd_rank INT, pts_rank INT, plus_minus_rank INT, nba_fantasy_pts_rank INT, dd2_rank INT, td3_rank INT, wnba_fantasy_pts_rank INT, cfid INT, cfparams VARCHAR(255), season_id VARCHAR(255));")
+# for i,row in df.iterrows():
+#     sql = "insert into nbastats values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+#     val = tuple(row)
+#     cursor.execute(sql, val)
+#     mydb.commit()
+#     print("record inserted")
 
 '''
-player_id INT
+This code creates the table
+#cursor.execute("CREATE TABLE nbastats(player_id VARCHAR(255), player_name VARCHAR(255), nickname VARCHAR(255), team_id VARCHAR(255), team_abbreviation VARCHAR(3), age VARCHAR(255), gp VARCHAR(255), w VARCHAR(255), l VARCHAR(255), w_pct VARCHAR(255), min VARCHAR(255), fgm VARCHAR(255), fga VARCHAR(255), fg_pct VARCHAR(255), fg3m VARCHAR(255), fg3a VARCHAR(255), fg3_pct VARCHAR(255), ftm VARCHAR(255), fta VARCHAR(255), ft_pct VARCHAR(255), oreb VARCHAR(255), dreb VARCHAR(255), reb VARCHAR(255), ast VARCHAR(255), tov VARCHAR(255), stl VARCHAR(255), blk VARCHAR(255), blka VARCHAR(255), pf VARCHAR(255), pfd VARCHAR(255), pts VARCHAR(255), plus_minus VARCHAR(255), nba_fantasy_pts VARCHAR(255), dd2 VARCHAR(255), td3 VARCHAR(255), wnba_fantasy_pts VARCHAR(255), gp_rank VARCHAR(255), w_rank VARCHAR(255), l_rank VARCHAR(255), w_pct_rank VARCHAR(255), min_rank VARCHAR(255), fgm_rank VARCHAR(255), fga_rank VARCHAR(255), fg_pct_rank VARCHAR(255), fg3m_rank VARCHAR(255), fg3a_rank VARCHAR(255), fg3_pct_rank VARCHAR(255), ftm_rank VARCHAR(255), fta_rank VARCHAR(255), ft_pct_rank VARCHAR(255), oreb_rank VARCHAR(255), dreb_rank VARCHAR(255), reb_rank VARCHAR(255), ast_rank VARCHAR(255), tov_rank VARCHAR(255), stl_rank VARCHAR(255), blk_rank VARCHAR(255), blka_rank VARCHAR(255), pf_rank VARCHAR(255), pfd_rank VARCHAR(255), pts_rank VARCHAR(255), plus_minus_rank VARCHAR(255), nba_fantasy_pts_rank VARCHAR(255), dd2_rank VARCHAR(255), td3_rank VARCHAR(255), wnba_fantasy_pts_rank VARCHAR(255), cfid VARCHAR(255), cfparams VARCHAR(255), season_id VARCHAR(255));")
+
+This code adds a single row to the table
+sql = "insert into nbastats values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+val = ("1", "stephen curry", "steph", "1", "gsw", "33", "82", "82", "0", "0.999", "33.42", "33.42", "33.42", "0.523", "33.42", "33.42", "0.523", "33.42", "33.42", "0.523", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "1", "1", "30.52", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2345", "1999")
+cursor.execute(sql, val)
+mydb.commit()
+'''
+
+# cursor.execute("CREATE TABLE nbastats (player_id INT, player_name VARCHAR(255), nickname VARCHAR(255), team_id VARCHAR(255), team_abbreviation VARCHAR(3), age INT, gp INT, w INT, l INT, w_pct DECIMAL, min DECIMAL, fgm DECIMAL, fga DECIMAL, fg_pct DECIMAL, fg3m DECIMAL, fg3a DECIMAL, fg3_pct DECIMAL, ftm DECIMAL, fta DECIMAL, ft_pct DECIMAL, oreb DECIMAL, dreb DECIMAL, reb DECIMAL, ast DECIMAL, tov DECIMAL, stl DECIMAL, blk DECIMAL, blka DECIMAL, pf DECIMAL, pfd DECIMAL, pts DECIMAL, plus_minus DECIMAL, nba_fantasy_pts DECIMAL, dd2 INT, td3 INT, wnba_fantasy_pts DECIMAL, gp_rank INT, w_rank INT, l_rank INT, w_pct_rank INT, min_rank INT, fgm_rank INT, fga_rank INT, fg_pct_rank INT, fg3m_rank INT, fg3a_rank INT, fg3_pct_rank INT, ftm_rank INT, fta_rank INT, ft_pct_rank INT, oreb_rank INT, dreb_rank INT, reb_rank INT, ast_rank INT, tov_rank INT, stl_rank INT, blk_rank INT, blka_rank INT, pf_rank INT, pfd_rank INT, pts_rank INT, plus_minus_rank INT, nba_fantasy_pts_rank INT, dd2_rank INT, td3_rank INT, wnba_fantasy_pts_rank INT, cfid INT, cfparams VARCHAR(255), season_id VARCHAR(255));")
+
+# cursor.execute("DROP TABLE nbastats")
+cursor.execute("SELECT player_name, pts FROM nbastats where player_name = %s", ("stephen curry",))
+# cursor.execute("DELETE FROM nbastats")
+myresult = cursor.fetchall()
+print(myresult)
+print(time.time() - t0)
+
+'''
+player_id VARCHAR(255)
 player_name VARCHAR(255)
 nickname VARCHAR(255)
-team_id INT
+team_id VARCHAR(255)
 team_abbreviation VARCHAR(3)
-age INT
-gp INT
-w INT
-l INT
-w_pct DECIMAL(3,3)
-min DECIMAL(5,2)
-fgm DECIMAL(5,2)
-fga DECIMAL(5,2)
-fg_pct DECIMAL(3,3)
-fg3m DECIMAL(5,2)
-fg3a DECIMAL(5,2)
-fg3_pct DECIMAL(3,3)
-ftm DECIMAL(5,2)
-fta DECIMAL(5,2)
-ft_pct DECIMAL(3,3)
-oreb DECIMAL(5,2)
-dreb DECIMAL(5,2)
-reb DECIMAL(5,2)
-ast DECIMAL(5,2)
-tov DECIMAL(5,2)
-stl DECIMAL(5,2)
-blk DECIMAL(5,2)
-blka DECIMAL(5,2)
-pf DECIMAL(5,2)
-pfd DECIMAL(5,2)
-pts DECIMAL(5,2)
-plus_minus DECIMAL(5,2)
-nba_fantasy_pts DECIMAL(5,2)
-dd2 INT
-td3 INT
-wnba_fantasy_pts DECIMAL(5,2)
-gp_rank INT
-w_rank INT
-l_rank INT
-w_pct_rank INT
-min_rank INT
-fgm_rank INT
-fga_rank INT
-fg_pct_rank INT
-fg3m_rank INT
-fg3a_rank INT
-fg3_pct_rank INT
-ftm_rank INT
-fta_rank INT
-ft_pct_rank INT
-oreb_rank INT
-dreb_rank INT
-reb_rank INT
-ast_rank INT
-tov_rank INT
-stl_rank INT
-blk_rank INT
-blka_rank INT
-pf_rank INT
-pfd_rank INT
-pts_rank INT
-plus_minus_rank INT
-nba_fantasy_pts_rank INT
-dd2_rank INT
-td3_rank INT
-wnba_fantasy_pts_rank INT
-cfid INT
+age VARCHAR(255)
+gp VARCHAR(255)
+w VARCHAR(255)
+l VARCHAR(255)
+w_pct VARCHAR(255)
+min VARCHAR(255)
+fgm VARCHAR(255)
+fga VARCHAR(255)
+fg_pct VARCHAR(255)
+fg3m VARCHAR(255)
+fg3a VARCHAR(255)
+fg3_pct VARCHAR(255)
+ftm VARCHAR(255)
+fta VARCHAR(255)
+ft_pct VARCHAR(255)
+oreb VARCHAR(255)
+dreb VARCHAR(255)
+reb VARCHAR(255)
+ast VARCHAR(255)
+tov VARCHAR(255)
+stl VARCHAR(255)
+blk VARCHAR(255)
+blka VARCHAR(255)
+pf VARCHAR(255)
+pfd VARCHAR(255)
+pts VARCHAR(255)
+plus_minus VARCHAR(255)
+nba_fantasy_pts VARCHAR(255)
+dd2 VARCHAR(255)
+td3 VARCHAR(255)
+wnba_fantasy_pts VARCHAR(255)
+gp_rank VARCHAR(255)
+w_rank VARCHAR(255)
+l_rank VARCHAR(255)
+w_pct_rank VARCHAR(255)
+min_rank VARCHAR(255)
+fgm_rank VARCHAR(255)
+fga_rank VARCHAR(255)
+fg_pct_rank VARCHAR(255)
+fg3m_rank VARCHAR(255)
+fg3a_rank VARCHAR(255)
+fg3_pct_rank VARCHAR(255)
+ftm_rank VARCHAR(255)
+fta_rank VARCHAR(255)
+ft_pct_rank VARCHAR(255)
+oreb_rank VARCHAR(255)
+dreb_rank VARCHAR(255)
+reb_rank VARCHAR(255)
+ast_rank VARCHAR(255)
+tov_rank VARCHAR(255)
+stl_rank VARCHAR(255)
+blk_rank VARCHAR(255)
+blka_rank VARCHAR(255)
+pf_rank VARCHAR(255)
+pfd_rank VARCHAR(255)
+pts_rank VARCHAR(255)
+plus_minus_rank VARCHAR(255)
+nba_fantasy_pts_rank VARCHAR(255)
+dd2_rank VARCHAR(255)
+td3_rank VARCHAR(255)
+wnba_fantasy_pts_rank VARCHAR(255)
+cfid VARCHAR(255)
 cfparams VARCHAR(255)
 season_id VARCHAR(255)
+'''
+
+'''
+player_id INT, player_name VARCHAR(255), nickname VARCHAR(255), team_id VARCHAR(255), team_abbreviation VARCHAR(3), age INT, gp INT, w INT, l INT, w_pct DECIMAL, min DECIMAL, fgm DECIMAL, fga DECIMAL, fg_pct DECIMAL, fg3m DECIMAL, fg3a DECIMAL, fg3_pct DECIMAL, ftm DECIMAL, fta DECIMAL, ft_pct DECIMAL, oreb DECIMAL, dreb DECIMAL, reb DECIMAL, ast DECIMAL, tov DECIMAL, stl DECIMAL, blk DECIMAL, blka DECIMAL, pf DECIMAL, pfd DECIMAL, pts DECIMAL, plus_minus DECIMAL, nba_fantasy_pts DECIMAL, dd2 INT, td3 INT, wnba_fantasy_pts DECIMAL, gp_rank INT, w_rank INT, l_rank INT, w_pct_rank INT, min_rank INT, fgm_rank INT, fga_rank INT, fg_pct_rank INT, fg3m_rank INT, fg3a_rank INT, fg3_pct_rank INT, ftm_rank INT, fta_rank INT, ft_pct_rank INT, oreb_rank INT, dreb_rank INT, reb_rank INT, ast_rank INT, tov_rank INT, stl_rank INT, blk_rank INT, blka_rank INT, pf_rank INT, pfd_rank INT, pts_rank INT, plus_minus_rank INT, nba_fantasy_pts_rank INT, dd2_rank INT, td3_rank INT, wnba_fantasy_pts_rank INT, cfid INT, cfparams VARCHAR(255), season_id VARCHAR(255)
+'''
+
+'''
+("1", "stephen curry", "steph", "1", "gsw", "33", "82", "82", "0", "0.999", "33.42", "33.42", "33.42", "0.523", "33.42", "33.42", "0.523", "33.42", "33.42", "0.523", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "12.52", "1", "1", "30.52", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2345", "1999")
 '''
 
 '''
